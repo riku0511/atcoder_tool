ec9ba667809ac516b90b5145eb3681e5ca2fe2ec
riku <ohtaki.riku@gmail.com>
2025-05-23 00:00:18 +0900

    250522

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Stage,Layer,Rect,Circle } from 'react-konva';
+import { Stage,Layer,Group,Rect,Circle,Text } from 'react-konva';
 import './index.css';
 
 const STAGE_WIDTH = 2100;
@@ -25,13 +25,14 @@ const createDragBoundCircle = (shapeWidth, shapeHeight) => {
 
 
 const Draw = () => {
-  const [rectangles, setRectangles] = useState([]);
-  const [circleangles, setCircleangles] = useState([]);
+  const [rects, setRects] = useState([]);
+  const [circle, setCircle] = useState([]);
 
     const handleAddRect = () => {
+    const id = rects.length;
     const newRect = {
-      id: rectangles.length,
-      x:  50 + rectangles.length * 120,
+      id,
+      x:  50 + rects.length * 120,
       y: 50,
       width: 100,
       height: 100,
@@ -39,23 +40,50 @@ const Draw = () => {
       stroke: 'black',
       strokeWidth: 2,
       draggable: true,
-      
+      text:`${id}`,
     };
-    setRectangles([...rectangles, newRect]);
+    setRects([...rects, newRect]);
   };
 
   const handleAddCircle = () => {
+    const id = circle.length;
     const newCircle = {
-      id: circleangles.length,
-      x:  50 + circleangles.length * 120,
+      id,
+      x:  50 + circle.length * 120,
       y: 150,
       radius:50,
       fill: 'red',
       stroke: 'black',
       strokeWidth: 2,
       draggable: true,
+      text:`${id}`,
     };
-    setCircleangles([...circleangles, newCircle]);
+    setCircle([...circle, newCircle]);
+  };
+
+  // クリックでテキスト編集
+  const handleEditTextRect = (id) => {
+    const old = rects.find(r => r.id === id);
+    const value = window.prompt('新しいテキストを入力してください', old.text);
+    if (value != null) {
+      setRects(rects.map(r =>
+        r.id === id
+          ? { ...r, text: value }
+          : r
+      ));
+    }
+  };
+
+  const handleEditTextCircle = (id) => {
+    const old = circle.find(r => r.id === id);
+    const value = window.prompt('新しいテキストを入力してください', old.text);
+    if (value != null) {
+      setCircle(circle.map(r =>
+        r.id === id
+          ? { ...r, text: value }
+          : r
+      ));
+    }
   };
 
   return (
@@ -65,21 +93,69 @@ const Draw = () => {
       <Stage
         width={STAGE_WIDTH}
         height={STAGE_HEIGHT}
-        pixelRatio={1}
+        
         className="stage-container"
       >
         <Layer>
-          {rectangles.map((rect) => (
-            <Rect key={rect.id} {...rect}
-            dragBoundFunc={createDragBoundRect(rect.width, rect.height)}
-            />
+          {rects.map(r => (
+            <Group
+              key={r.id}
+              x={r.x}
+              y={r.y}
+              draggable
+              dragBoundFunc={createDragBoundRect(r.width, r.height)}
+              onClick={() => handleEditTextRect(r.id)}
+            >
+              <Rect
+                width={r.width}
+                height={r.height}
+                fill={r.fill}
+                stroke={r.stroke}
+                strokeWidth={r.strokeWidth}
+              />
+              <Text
+                text={r.text}
+                fontSize={18}
+                fill="black"
+                width={r.width}
+                height={r.height}
+                align="center"
+                verticalAlign="middle"
+              />
+            </Group>
           ))}
-          {circleangles.map((circle) => (
-            <Circle
-            key={circle.id} {...circle}
-            dragBoundFunc={createDragBoundCircle(circle.radius , circle.radius)}
-            />
+          
+          {circle.map(r => (
+            <Group
+              key={r.id}
+              x={r.x}
+              y={r.y}
+              draggable
+              dragBoundFunc={createDragBoundCircle(r.radius, r.radius)}
+              onClick={() => handleEditTextCircle(r.id)}
+            >
+              <Circle
+                radius={r.radius}
+                fill={r.fill}
+                stroke={r.stroke}
+                strokeWidth={r.strokeWidth}
+                x={0}
+                y={0}
+              />
+              <Text
+                text={r.text}
+                fontSize={18}
+                fill="black"
+                width={r.radius*2}
+                height={r.radius*2}
+                align="center"
+                verticalAlign="middle"
+                x={-r.radius}
+                y={-r.radius}
+              />
+            </Group>
           ))}
+          
         </Layer>
       </Stage>
     </div>
