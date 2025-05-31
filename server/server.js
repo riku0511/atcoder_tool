5ac9dc44071b3a89356adbb87e8304ec75e1f143
riku <ohtaki.riku@gmail.com>
2025-05-31 23:29:18 +0900

    250531

--- server/server.js
+++ server/server.js
@@ -12,17 +12,26 @@ app.get('/api/fetch-problem', async (req, res) => {
   if (!url) return res.status(400).json({ error: 'url required' });
 
   try {
+    //テキスト状態でhtml読み込み
     const html = await fetch(url).then(r => r.text());
+    //DOM変換
     const $ = cheerio.load(html);
-
-    const title = $('.lang-en h1').first().text().trim();
-    const statement = $('.lang-en .part:has(h3:contains("Statement"))').html();
+    
+    const title = $('title').first().text().trim();
+    const statement = $('.lang-ja .part:has(h3:contains("問題文"))').html();
     const samples = [];
+    let temp = {};
 
-    $('#task-statement section.sample').each((i, sec) => {
-      const input = $(sec).find('pre').first().text();
-      const output = $(sec).find('pre').eq(1).text();
-      samples.push({ input, output });
+    $('#task-statement .lang-ja .part:has(h3:contains("例")) ').each((index, sec) => {
+      const text = $(sec).find('pre').text().trim();
+      if (index % 2 === 0) {
+        temp.input = text;
+      }
+      else {
+        temp.output = text;
+        samples.push({ ...temp });
+        temp = {}; 
+      }
     });
 
     res.json({ title, statement, samples });
